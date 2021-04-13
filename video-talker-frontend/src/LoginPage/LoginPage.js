7145760c6368fbf7737dd614a9957e7fefcc94c4
tal1992 <timothylam92@hotmail.co.uk>
2021-04-13 15:15:56 +0100

    Ceating Login Page

--- video-talker-frontend/src/LoginPage/LoginPage.js
+++ video-talker-frontend/src/LoginPage/LoginPage.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import logo from '../resources/logo.png'
+import UsernameInput from './components/UsernameInput';
+import SubmitBuuton from './components/SubmitButton';
+import { useHistory } from 'react-router-dom';
+import { setUsername } from '../store/actions/dashboardActions';
+import './LoginPage.css';
 
-const LoginPage = () => {
+
+
+const LoginPage = ({ saveUsername }) => {
+    const [username, setUsername] = useState('');
+
+    const history = useHistory();
+
+    const handleSubmitButtonPressed =() => {
+        history.push('./dashboard')
+        saveUsername(username);
+    };
+
     return (
-        <div>
-            Login Page
+        <div className='login-page_container background_main_color'>
+             <div className='login-page_login_box background_secondary_color'>
+                 <div className='login-page_logo_container'>
+                    <img className='login-page_logo_image' src={logo} alt='VideoTalker' />
+                 </div>
+                 <div className='login-page_title_container'>
+                    <h2>Get on Board</h2>
+                 </div>
+                 <UsernameInput username={username} setUsername={setUsername}/>
+                 <SubmitBuuton handleSubmitButtonPressed={handleSubmitButtonPressed}/>
+            </div>
         </div>
     );
 };
 
-export default LoginPage;
+const mapActionToProps = (dispatch) => {
+    return {
+        saveUsername: username => dispatch(setUsername(username))
+    };
+};
+
+export default connect(null, mapActionToProps)(LoginPage);
 
