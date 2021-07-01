d0125982db563da53c39fcd0dccbe921ed26cac4
tal1992 <timothylam92@hotmail.co.uk>
2021-07-01 20:33:17 +0100

    Deploying backend and frontend application

--- video-talker-frontend/src/utils/wssConnection/wssConnection.js
+++ video-talker-frontend/src/utils/wssConnection/wssConnection.js
@@ -4,7 +4,7 @@ import * as dashboardActions from '../../store/actions/dashboardActions';
 import * as webRTCHandler from '../webRTC/webRTCHandler';
 import * as webRTCGroupCallHandler from '../webRTC/webRTCGroupCallHandler';
 
-const SERVER = 'https://videotalkerserver.herokuapp.com/';
+const SERVER = 'https://videotalkerbackendserver.herokuapp.com/';
 
 const broadcastEventTypes = {
   ACTIVE_USERS: 'ACTIVE_USERS',
